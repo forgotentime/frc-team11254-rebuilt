059ebfd143d257d809b88da2d0033c2fda20c0b6
forgotentime <michaelacovington@hotmail.com>
2026-01-14 18:31:22 -0500

    skeleton code done

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,31 +6,50 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.*;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.*;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.XboxController;
 import static frc.robot.Constants.shooterConstants.*;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 
 public class Shooter extends SubsystemBase {
-  private SparkMax motor;
-  private SparkMaxConfig motorconfig;
+  private SparkMax motorShooter;
+  private SparkMaxConfig motorShooterConfig;
+  private SparkClosedLoopController sparkControl;
+  private RelativeEncoder encoder;
   
   /** Creates a new Shooter. */
   public Shooter() {
-    motor = new SparkMax(SHOOTER, MotorType.kBrushless);
-    motorconfig = new SparkMaxConfig();
+    motorShooter = new SparkMax(SHOOTER, MotorType.kBrushless);
+    motorShooterConfig = new SparkMaxConfig();
+    sparkControl = motorShooter.getClosedLoopController();
+    encoder = motorShooter.getEncoder();
 
-    motorconfig
+    motorShooterConfig
       .idleMode(IdleMode.kBrake)
-      .smartCurrentLimit(60);
-      motor.configure(motorconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
+      .smartCurrentLimit(60)
+      .closedLoop.positionWrappingEnabled(true)
+      .pid(.005, 0, 0);
 
+    motorShooter.configure(motorShooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+public void stop(){
+  motorShooter.stopMotor();
+}
+public void spinShoot(){
+  motorShooter.set(.4);
+}
+public void PIDShoot(double fireSpeed){
+  sparkControl.setSetpoint(fireSpeed, ControlType.kVelocity);
+}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
